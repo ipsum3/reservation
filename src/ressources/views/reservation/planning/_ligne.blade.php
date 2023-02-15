97e79b083d438aac20ae77ff71ad7deeaf958921
PANOR Ismael <i.panor@pixell.eu>
2023-02-15 17:26:33 -0400

    Ajout icone lock si vehicule bloqué dans planning

--- src/ressources/views/reservation/planning/_ligne.blade.php
+++ src/ressources/views/reservation/planning/_ligne.blade.php
@@ -52,6 +52,8 @@
                         >
                             @if ($resa->has_conflit)
                                 <i class="fa fa-exclamation-triangle"></i>
+                            @elseif( $resa->vehicule_blocage )
+                                <i class="fa fa-lock"></i>
                             @endif
                             {{ $resa->prenom.' '.$resa->nom }}
                         </a>
