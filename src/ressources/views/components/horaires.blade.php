1daf62e0330fcf0dfe25943c13deee046f599784
AxelRN <robertnaudinaxel@gmail.com>
2023-11-20 17:36:59 -0400

    correction créneaux horaires

--- src/ressources/views/components/horaires.blade.php
+++ src/ressources/views/components/horaires.blade.php
@@ -1,19 +1,18 @@
 @props(['min' => '7:00', 'max' => '21:00', 'step' => 30, 'value' => '16:00'])
 
 @php
+    use Carbon\Carbon;
 
-$name = $attributes->get('name');
-
+    $name = $attributes->get('name');
+    $minTime = Carbon::parse($min);
+    $maxTime = Carbon::parse($max);
 @endphp
 
-
 <select {{ $attributes->merge([]) }}>
-    @for ($heure = (int) $min; $heure <= (int) $max; $heure++)
-        @for ($minute = 0; $minute <= $step; $minute = $minute + 30)
-            <option value="{{{ str_pad($heure, 2, '0', STR_PAD_LEFT) }}}:{{{ str_pad($minute, 2, '0', STR_PAD_LEFT) }}}"
-                    {{{ old($name, $value) == str_pad($heure, 2, '0', STR_PAD_LEFT).':'.str_pad($minute, 2, '0', STR_PAD_LEFT) ? 'selected' : '' }}}>
-                {{{ $heure }}}h{{{ str_pad($minute, 2, '0', STR_PAD_LEFT) }}}
-            </option>
-        @endfor
+    @for ($time = $minTime->copy(); $time <= $maxTime; $time->addMinutes($step))
+        <option value="{{ $time->format('H:i') }}"
+                {{ old($name, $value) == $time->format('H:i') ? 'selected' : '' }}>
+            {{ $time->format('H\hi') }}
+        </option>
     @endfor
 </select>
