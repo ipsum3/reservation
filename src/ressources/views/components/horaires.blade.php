52341605792a5f41a9c2ab3fe7352accce223f7d
AxelRN <robertnaudinaxel@gmail.com>
2023-11-28 10:21:56 -0400

    amélioration créneaux horaires

--- src/ressources/views/components/horaires.blade.php
+++ src/ressources/views/components/horaires.blade.php
@@ -4,15 +4,43 @@
     use Carbon\Carbon;
 
     $name = $attributes->get('name');
-    $minTime = Carbon::parse($min);
     $maxTime = Carbon::parse($max);
-@endphp
+    $minTime = Carbon::parse($min);
 
+    $minHour = $minTime->hour;
+    $minMinute = $minTime->minute;
+
+    $adjustedMinHour = $minHour;
+    $adjustedMinMinute = ceil($minMinute / $step) * $step;
+    if ($adjustedMinMinute >= 60) {
+        $adjustedMinHour++;
+        $adjustedMinMinute = 0;
+    }
+    $minTime->hour($adjustedMinHour)->minute($adjustedMinMinute);
+    $options = [];
+    @endphp
 <select {{ $attributes->merge([]) }}>
-    @for ($time = $minTime->copy(); $time <= $maxTime; $time->addMinutes($step))
+    @if( Carbon::parse($min) != $minTime )
+        <option value="{{ Carbon::parse($min)->format('H:i') }}"
+                {{ old($name, $value) == Carbon::parse($min)->format('H:i') ? 'selected' : '' }}>
+            {{ Carbon::parse($min)->format('H\hi') }}
+        </option>
+    @endif
+
+    @for ($time= $minTime->copy(); $time <= $maxTime; $time->addMinutes($step))
         <option value="{{ $time->format('H:i') }}"
                 {{ old($name, $value) == $time->format('H:i') ? 'selected' : '' }}>
             {{ $time->format('H\hi') }}
         </option>
+        @php
+            $options[] = $time->format('H\hi');
+        @endphp
     @endfor
+
+    @if( !in_array($maxTime->format('H\hi'), $options) )
+        <option value="{{ $maxTime->format('H:i') }}"
+                {{ old($name, $value) == $maxTime->format('H:i') ? 'selected' : '' }}>
+            {{ $maxTime->format('H\hi') }}
+        </option>
+    @endif
 </select>
