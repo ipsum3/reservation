aa4dd1cca43e26724feee2a7731b98dafdfb2f00
B1nj <postmaster@b1nj.fr>
2024-04-10 14:43:54 -0400

    fix custom fields

--- src/ressources/views/tarif/saison/form.blade.php
+++ src/ressources/views/tarif/saison/form.blade.php
@@ -45,7 +45,7 @@
                 @foreach(config('ipsum.reservation.saison.custom_fields') as $field)
                     @php
                         $field_name = 'custom_fields['.$field['name'].']';
-                        $field_value = old('custom_fields.'.$field['name'], $saison->custom_fields->{$field['name']} ?  : ($field['type'] == "repeater" ? [] : '') );
+                        $field_value = old('custom_fields.'.$field['name'], $saison->custom_fields->{$field['name']} ?? ($field['type'] == "repeater" ? [] : '') );
                     @endphp
                     <x-admin::custom :field="$field" :name="$field_name" :value="$field_value"/>
                 @endforeach
