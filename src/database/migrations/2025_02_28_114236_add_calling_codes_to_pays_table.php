4526e73db015048b3ef7e012b46e03cf240eb56d
AxelRN <robertnaudinaxel@gmail.com>
2025-03-06 09:10:38 -0400

    callingCodes + seeder

---
+++ src/database/migrations/2025_02_28_114236_add_calling_codes_to_pays_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Ipsum\Reservation\app\Models\Reservation\Pays;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Vider la table "pays"
+        DB::table('pays')->truncate();
+
+        Schema::table('pays', function (Blueprint $table) {
+            $table->string('callingCodes')->nullable()->after('nom');
+        });
+
+        // Relancer le seeder "PaysTableSeeder"
+        Artisan::call('db:seed', [
+            '--class' => 'PaysTableSeeder'
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+};
