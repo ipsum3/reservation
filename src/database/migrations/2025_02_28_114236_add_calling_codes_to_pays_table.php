bcf7f6c7e9ba532bd3588a9f65ef7c2f552d9f80
AxelRN <robertnaudinaxel@gmail.com>
2025-03-18 14:41:33 -0400

    correction pays

--- src/database/migrations/2025_02_28_114236_add_calling_codes_to_pays_table.php
+++ src/database/migrations/2025_02_28_114236_add_calling_codes_to_pays_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
         // Relancer le seeder "PaysTableSeeder"
         Artisan::call('db:seed', [
             '--class' => \Ipsum\Reservation\database\seeds\PaysTableSeeder::class,
+            '--force' => true,
         ]);
     }
 
