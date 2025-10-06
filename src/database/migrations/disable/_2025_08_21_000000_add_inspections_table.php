bcae5be49c025729ad5caf74c1e12f5203003d84
AxelRN <robertnaudinaxel@gmail.com>
2025-10-06 09:35:53 -0400

    seed dans la migration pour l'état des lieux

--- src/database/migrations/disable/_2025_08_21_000000_add_inspections_table.php
+++ src/database/migrations/disable/_2025_08_21_000000_add_inspections_table.php
@@ -4,6 +4,10 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Ipsum\Reservation\app\Models\Client;
+use Ipsum\Reservation\app\Models\Dommage\Element;
+use Ipsum\Reservation\app\Models\Dommage\Emplacement;
+use Ipsum\Reservation\app\Models\Inspection\Checklist;
+use Ipsum\Reservation\app\Models\Inspection\Type;
 
 return new class extends Migration
 {
@@ -25,7 +29,7 @@ return new class extends Migration
             $table->foreignId('admin_id')->index()->unsigned();
             $table->foreignId('type_id')->index()->unsigned();
             $table->integer('kilometrage')->nullable();
-            $table->string('carburant')->nullable();
+            $table->tinyInteger('carburant')->nullable();
             $table->text('locataire_signature')->nullable();
             $table->timestamp('locataire_signature_at')->nullable();
             $table->text('agent_signature')->nullable();
@@ -40,6 +44,17 @@ return new class extends Migration
             $table->string('nom');
         });
 
+        $inspection_types = [
+            'Initial',
+            'Final'
+        ];
+
+        foreach ($inspection_types as $type) {
+            Type::create([
+                'nom' => $type,
+            ]);
+        }
+
         Schema::create('checklist-inspections', function (Blueprint $table) {
             $table->id();
             $table->foreignId('checklist_id')->index()->unsigned();
@@ -53,6 +68,22 @@ return new class extends Migration
             $table->string('nom');
             $table->tinyInteger('order')->default(0);
         });
+
+        $checklists = [
+            'Propreté intérieure',
+            'Propreté extérieure',
+            'Papiers du véhicule',
+            'Kit de sécurité',
+            'Niveaux',
+            'Roue de secours',
+        ];
+
+        foreach ($checklists as $index => $nom) {
+            Checklist::create([
+                'nom' => $nom,
+                'order' => $index + 1,
+            ]);
+        }
 
         // Table dommages constatés
         Schema::create('dommages', function (Blueprint $table) {
@@ -74,6 +105,22 @@ return new class extends Migration
             $table->tinyInteger('order')->default(0);
         });
 
+        $dommage_types = [
+            'Rayure',
+            'Enfoncement / bosselage',
+            'Cassure',
+            'Éclats / bris',
+            'Pièce manquante',
+            'Autre'
+        ];
+
+        foreach ($dommage_types as $index => $nom) {
+            \Ipsum\Reservation\app\Models\Dommage\Type::create([
+                'nom' => $nom,
+                'order' => $index + 1
+            ]);
+        }
+
         // Table éléments pouvant avoir un dommage
         Schema::create('dommage_elements', function (Blueprint $table) {
             $table->id();
@@ -81,12 +128,42 @@ return new class extends Migration
             $table->tinyInteger('order')->default(0);
         });
 
+        $elements = [
+            'Carrosserie',
+            'Vitrage',
+            'Éclairage',
+            'Accessoires et divers'
+        ];
+
+        foreach ($elements as $index => $nom) {
+            Element::create([
+                'nom' => $nom,
+                'order' => $index + 1
+            ]);
+        }
+
         // Table emplacements du véhicule
         Schema::create('dommage_emplacements', function (Blueprint $table) {
             $table->id();
             $table->string('nom');
             $table->tinyInteger('order')->default(0);
         });
+
+        $emplacements =  [
+            'Avant',
+            'Avant droit',
+            'Avant gauche',
+            'Arrière droit',
+            'Arrière gauche',
+            'Arrière'
+        ];
+
+        foreach ($emplacements as $index => $nom) {
+            Emplacement::create([
+                'nom' => $nom,
+                'order' => $index + 1
+            ]);
+        }
 
     }
 
