95b4b620249551bf0a8b7300f27b21be04092f75
Benjamin <postmaster@b1nj.fr>
2025-12-22 07:51:58 -0400

    sauvegarde

--- src/app/Console/Commands/PlanningOptimiser.php
+++ src/app/Console/Commands/PlanningOptimiser.php
@@ -41,12 +41,42 @@ class PlanningOptimiser extends Command
             $query->where('categorie_id', $this->option('categorie'));
         }
 
+        /*$sauvegarde = $query->get()->map(function (Reservation $reservation) {
+            return [
+                'id' => $reservation->id,
+                'vehicule_id' => $reservation->vehicule_id,
+                'categorie_id' => $reservation->categorie_id,
+                'debut_at' => $reservation->debut_at,
+                'fin_at' => $reservation->fin_at
+            ];
+        })->groupBy('categorie_id');
+
+        cache()->put('reservation_optimisation_sauvegarde',  $sauvegarde);*/
+
         $query->update(['vehicule_id' => null]);
+
+
+        $planning = Planning::createByReservations();
+
+        $planning = new Planning($ressources, $reservations, $blocages);
+        $planning->optimiser();
+        $planning->getTauxOptimisation();
+        $resas = $planning->getReservations();
+
 
         $reservations = $query->get();
 
         foreach ($reservations as $reservation) {
             $reservation->save();
+        }
+
+
+        foreach (cache()->get('reservation_optimisation_sauvegarde') as $categories) {
+            foreach ($categories as $reservation) {
+                $resa = Reservation::find($reservation['id']);
+                $resa->vehicule_id = $reservation['vehicule_id'];
+                $resa->save();
+            }
         }
 
 
